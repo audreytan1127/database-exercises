c5f5056267b84f614dbb61f9639f9825a5cb9cab
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-17 16:01:38 -0500

    completed mysql exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
-USE codeup_test_db;
+    USE codeup_test_db;
+
+    DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(100) NOT NULL,
+    name VARCHAR(200) NOT NULL,
+    release_date INT NOT NULL,
+    sales DECIMAL(9, 2) NOT NULL,
+    genre VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+)
