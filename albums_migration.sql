9ed833c8b2cdde6d36e59063e7ae0665eee087a8
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-18 09:29:10 -0500

    adjusted varchar values and added unsigned

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,9 +5,9 @@
 CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(100) NOT NULL,
-    name VARCHAR(200) NOT NULL,
-    release_date INT NOT NULL,
-    sales DECIMAL(9, 2) NOT NULL,
-    genre VARCHAR(30) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    release_date INT UNSIGNED NOT NULL,
+    sales DECIMAL(7, 2) UNSIGNED NOT NULL,
+    genre VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
-)
+);
