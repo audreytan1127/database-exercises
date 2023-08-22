c68e0e523a216a5accf445f7090fea8a4328e213
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-22 15:39:18 -0500

    completed codeup join exercise for mysql without class walk thru and bonus

---
+++ practice_mysql.sql
@@ -0,0 +1,53 @@
+CREATE TABLE roles (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       PRIMARY KEY (id)
+);
+
+CREATE TABLE users (
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       name VARCHAR(100) NOT NULL,
+                       email VARCHAR(100) NOT NULL,
+                       role_id INT UNSIGNED DEFAULT NULL,
+                       PRIMARY KEY (id),
+                       FOREIGN KEY (role_id) REFERENCES roles (id)
+);
+
+INSERT INTO roles (name) VALUES ('admin');
+INSERT INTO roles (name) VALUES ('author');
+INSERT INTO roles (name) VALUES ('reviewer');
+INSERT INTO roles (name) VALUES ('commenter');
+
+INSERT INTO users (name, email, role_id) VALUES
+                                             ('bob', 'bob@example.com', 1),
+                                             ('joe', 'joe@example.com', 2),
+                                             ('sally', 'sally@example.com', 3),
+                                             ('adam', 'adam@example.com', 3),
+                                             ('jane', 'jane@example.com', null),
+                                             ('mike', 'mike@example.com', null);
+
+
+INSERT INTO users (name, email, role_id) VALUES
+                                             ('jess', 'jess@example.com', 2),
+                                             ('leah', 'leah@example.com', 2),
+                                             ('mark', 'mark@example.com', 2),
+                                             ('terry', 'terry@example.com', null);
+
+# LEFT JOIN will get all users, even if they do not have a role assigned to them.
+SELECT users.name AS user_name,
+       roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
+
+# RIGHT JOIN will get all roles, even if there is not a user assigned to it.
+SELECT users.name AS user_name,
+       roles.name AS role_name
+FROM users
+         RIGHT JOIN roles ON users.role_id = roles.id;
+
+# JOIN / INNER JOIN will get the users that are assigned a role.
+# Will not show user or role that are not assigned with its counterpart.
+SELECT users.name AS user_name,
+       roles.name AS role_name
+FROM users
+         JOIN roles ON users.role_id = roles.id;
