9f90455ce375610a0a2fb8dcf67eb4f4de0c6383
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-23 10:37:08 -0500

    completed bonus with class walk thru

--- join_exercises.sql
+++ join_exercises.sql
@@ -35,9 +35,9 @@ GROUP BY t.title;
 
 
 # Find current salary of each department manager
-SELECT d.dept_name AS "Department Name",
+SELECT d.dept_name                            AS "Department Name",
        CONCAT(e.first_name, ' ', e.last_name) AS "Department Manager",
-       s.salary AS Salary
+       s.salary                               AS Salary
 FROM employees as e
          JOIN dept_manager dm
               ON dm.emp_no = e.emp_no
@@ -46,6 +46,23 @@ FROM employees as e
          JOIN salaries s
               ON s.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01'
-AND s.to_date = '9999-01-01'
+  AND s.to_date = '9999-01-01'
 ORDER BY dept_name;
+
+# BONUS!
+SELECT CONCAT(e.first_name, ' ', e.last_name)               AS Employee,
+       d.dept_name                                          AS "Department",
+       CONCAT(managers.first_name, ' ', managers.last_name) AS Manager
+FROM employees e
+         JOIN dept_emp de
+              ON e.emp_no = de.emp_no
+         JOIN departments d
+              ON de.dept_no = d.dept_no
+         JOIN dept_manager dm
+              ON d.dept_no = dm.dept_no
+         JOIN employees managers
+              ON dm.emp_no = managers.emp_no
+WHERE de.to_date = '9999-01-01'
+AND dm.to_date = '9999-01-01'
+ORDER BY d.dept_name, e.emp_no;
 
